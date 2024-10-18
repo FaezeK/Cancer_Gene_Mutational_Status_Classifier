59fecf076509e79ca00f550f9cb84c776be30914
Faeze Keshavarz <fkeshavarz@gphost04.bcgsc.ca>
2024-10-17 17:44:18 -0700

    preprocessing script completed

--- Snakefile
+++ Snakefile
@@ -8,16 +8,21 @@ rule preprocess_data:
         pog_tpm = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/pog/hg38/pog_tpm.tsv',
         pog_snv = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/pog/hg38/new_download/pog_snps_indels_short.tsv',
         pog_germline = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/pog/germline_mut/POG500.germline_variants.final.xlsx',
+        pog_cnv = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/pog/hg38/cnv/all_CN.tsv',
+        pog_sv = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/pog/pog_sv.tsv',
+
         tcga_tpm = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/tcga/hg38/tcga_rsem_gene_tpm.txt',
         tcga_snv = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/tcga/hg38/tcga_gdc_hg38_mut_fltr.txt',
         tcga_germline = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/tcga/germline_mut/PCA_pathVar_integrated_filtered_adjusted.tsv',
         tcga_cnv = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/tcga/cnv/TCGA.PANCAN.sampleMap_Gistic2_CopyNumber_Gistic2_all_data_by_genes',
         tcga_sv = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/tcga/sv/all_sv.txt',
-        tcga_t_type = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/tcga/tcga_t_type.tsv',
         all_genes = '/projects/fkeshavarz_prj/fkeshavarz_scratch/data/all_genes.txt'
     output:
         pog_tpm_prcssd = 'tmp_data/pog_tpm_prcssd.tsv',
         pog_mut_prcssd = 'tmp_data/pog_mut_prcssd.tsv',
+        pog_cnv_prcssd = 'tmp_data/pog_cnv_prcssd.tsv',
+        pog_sv_prcssd = 'tmp_data/pog_sv_prcssd.tsv',
+
         tcga_tpm_prcssd = 'tmp_data/tcga_tpm_prcssd.tsv',
         tcga_mut_prcssd = 'tmp_data/tcga_mut_prcssd.tsv',
         tcga_cnv_prcssd = 'tmp_data/tcga_cnv_prcssd.tsv',
--- src/process_input.py
+++ src/process_input.py
@@ -34,12 +34,13 @@ pog_germline = pd.read_excel(snakemake.input.pog_germline)
 
 # copy number variation data
 tcga_cnv = pd.read_csv(snakemake.input.tcga_cnv, delimiter = '\t', header=0)
+pog_cnv = pd.read_csv(snakemake.input.pog_cnv, delimiter = '\t', header=0)
 
 # structural variation data
 tcga_sv = pd.read_csv(snakemake.input.tcga_sv, delimiter='\t', header=0)
+pog_sv = pd.read_csv(snakemake.input.pog_sv, delimiter='\t', header=0)
 
 # metadata
-tcga_t_type = pd.read_csv(snakemake.input.tcga_t_type, delimiter='\t', header=0)
 all_genes = pd.read_csv(snakemake.input.all_genes, delimiter='\t', header=0)
 print('Input data are read!')
 print('')
@@ -59,14 +60,14 @@ def assigne_ensembl_id_as_index(expr_df):
     expr_df = expr_df.set_index('genes')
     return expr_df
 
-# harmonize gene names in all three expression matrices
-# change the sample column name to gene_id to match with the other two datasets
+# harmonize gene names in expression matrices
+# change the sample column name to gene_id to match with the POG dataset
 tcga_tpm = tcga_tpm.rename(columns={"sample":"gene_id"})
 
 pog_tpm = assigne_ensembl_id_as_index(pog_tpm)
 tcga_tpm = assigne_ensembl_id_as_index(tcga_tpm)
 
-# remove the genes that do not exist in TCGA from POG and Hartwig expression data
+# remove the genes that do not exist in TCGA from POG expression data
 pog_tpm = pog_tpm[pog_tpm.index.isin(tcga_tpm.index)]
 
 # exclue non-primary tumours from TCGA (now there are two sets of TCGA
@@ -78,92 +79,85 @@ tcga_tpm_primary_only = tcga_tpm.loc[:, tcga_tpm.columns.str.slice(13,15)=='01']
 tcga_tpm = (2**tcga_tpm) - 0.001
 tcga_tpm_primary_only = (2**tcga_tpm_primary_only) - 0.001
 
+#########################################
+######### Process CNV Datasets ##########
+#########################################
+
+# change the cnv data format of POG to match TCGA
+pog_cnv = pog_cnv.set_index('p_id')
+pog_cnv_T = pog_cnv.T
+pog_cnv_T.insert(0, 'gene_id',pog_cnv_T.index)
+pog_cnv_T = pog_cnv_T.reset_index(drop=True)
+
+# add gene names to pog cnv
+pog_cnv = assigne_ensembl_id_as_index(pog_cnv_T)
+pog_cnv['gene_name'] = pog_cnv.index.str.split('_').str[0]
+pog_cnv = pog_cnv.set_index('gene_name')
+
+# assign genes as index for tcga cnv
+tcga_cnv = tcga_cnv.set_index('Sample')
+
 #################################################
-######### Process SNVs/INDELS Datasets ##########
+######### Process SNVs/INDELs Datasets ##########
 #################################################
-
-# for samples with multiple mutations in the same gene, get the most important one
-# order of importance/harmfulness: 
-# frameshift_variant > stop_gained > start_lost > stop_lost > splice_acceptor_variant > splice_donor_variant > missense_variant > 
-# inframe_deletion > inframe_insertion > protein_altering_variant > splice_region_variant > incomplete_terminal_codon_variant > 
-# stop_retained_variant > synonymous_variant > 3_prime_UTR_variant > 5_prime_UTR_variant > regulatory_region_variant > coding_sequence_variant 
-# > intron_variant > upstream_gene_variant > downstream_gene_variant > non_coding_transcript_exon_variant > mature_miRNA_variant
-tcga_snv['conseq_score'] = 0
-tcga_snv.loc[tcga_snv.One_Consequence=='frameshift_variant', 'conseq_score'] = 1
-tcga_snv.loc[tcga_snv.One_Consequence=='stop_gained', 'conseq_score'] = 2
-tcga_snv.loc[tcga_snv.One_Consequence=='start_lost', 'conseq_score'] = 3
-tcga_snv.loc[tcga_snv.One_Consequence=='stop_lost', 'conseq_score'] = 4
-tcga_snv.loc[tcga_snv.One_Consequence=='splice_acceptor_variant', 'conseq_score'] = 5
-tcga_snv.loc[tcga_snv.One_Consequence=='splice_donor_variant', 'conseq_score'] = 6
-tcga_snv.loc[tcga_snv.One_Consequence=='missense_variant', 'conseq_score'] = 7
-tcga_snv.loc[tcga_snv.One_Consequence=='inframe_deletion', 'conseq_score'] = 8
-tcga_snv.loc[tcga_snv.One_Consequence=='inframe_insertion', 'conseq_score'] = 9
-tcga_snv.loc[tcga_snv.One_Consequence=='protein_altering_variant', 'conseq_score'] = 10
-tcga_snv.loc[tcga_snv.One_Consequence=='splice_region_variant', 'conseq_score'] = 11
-tcga_snv.loc[tcga_snv.One_Consequence=='incomplete_terminal_codon_variant', 'conseq_score'] = 12
-tcga_snv.loc[tcga_snv.One_Consequence=='stop_retained_variant', 'conseq_score'] = 13
-tcga_snv.loc[tcga_snv.One_Consequence=='synonymous_variant', 'conseq_score'] = 14
-tcga_snv.loc[tcga_snv.One_Consequence=='3_prime_UTR_variant', 'conseq_score'] = 15
-tcga_snv.loc[tcga_snv.One_Consequence=='5_prime_UTR_variant', 'conseq_score'] = 16
-tcga_snv.loc[tcga_snv.One_Consequence=='regulatory_region_variant', 'conseq_score'] = 17
-tcga_snv.loc[tcga_snv.One_Consequence=='coding_sequence_variant', 'conseq_score'] = 18
-tcga_snv.loc[tcga_snv.One_Consequence=='intron_variant', 'conseq_score'] = 19
-tcga_snv.loc[tcga_snv.One_Consequence=='upstream_gene_variant', 'conseq_score'] = 20
-tcga_snv.loc[tcga_snv.One_Consequence=='downstream_gene_variant', 'conseq_score'] = 21
-tcga_snv.loc[tcga_snv.One_Consequence=='non_coding_transcript_exon_variant', 'conseq_score'] = 22
-tcga_snv.loc[tcga_snv.One_Consequence=='mature_miRNA_variant', 'conseq_score'] = 23
-
-tcga_snv = tcga_snv.sort_values(by=['Tumor_Sample_Barcode','Hugo_Symbol','conseq_score'])
-
-# for the same sample, same gene, keep the first row
-tcga_snv = tcga_snv.drop_duplicates(subset=['Tumor_Sample_Barcode', 'Hugo_Symbol'], keep='first')
 
 # change/create the patient id column to match across all three mutation datasets
 pog_snv = pog_snv.rename(columns={"patient.participant_project_identifier":"p_id"})
 tcga_snv['p_id'] = tcga_snv.Tumor_Sample_Barcode.str.slice(0,15)
 
-# convert gene name column in TCGA mutation dataframe to match POG 
-tcga_snv = tcga_snv.rename(columns={'Hugo_Symbol':'gene_name'})
+# harmonize the column names for SNV datasets
+tcga_snv = tcga_snv.rename(columns={'Hugo_Symbol':'gene_name', 'One_Consequence':'consequence', 'HGVSc':'base_change', 
+                                    'HGVSp':'amino_acid_change'})
 
-# remove the samples from the same patient that has the same gene mutation
-tcga_snv = tcga_snv.drop_duplicates(subset=['p_id', 'gene_name', 'One_Consequence'], keep='first')
+pog_snv = pog_snv.rename(columns={'patient.participant_project_identifier':'p_id', 'effect':'consequence', 
+                                  'amino_acid_c':'base_change', 'amino_acid_p':'amino_acid_change'})
+
+# remove the samples that for the same patient has the same gene mutation
+tcga_snv = tcga_snv.drop_duplicates(subset=['p_id', 'gene_name', 'consequence'], keep='first')
+pog_snv = pog_snv.drop_duplicates(subset=['consequence','impact','gene_name','p_id'])
 
 # find samples that have expression, mutation and CNV data
-def find_smpls_w_expr_n_mut_n_cnv_data(expr_df, mut_df, cnv_df):
-    common_ids = list(set(mut_df.p_id) & set(cnv_df.columns) & set(expr_df.columns))
+def find_smpls_w_expr_n_mut_n_cnv_data(expr_df, snv_df, cnv_df):
+    common_ids = list(set(snv_df.p_id) & set(expr_df.columns) & set(cnv_df.columns))
     
-    mut_df = mut_df[mut_df.p_id.isin(common_ids)]
-    cnv_df = cnv_df.set_index('Sample')
-    cnv_df = cnv_df.loc[:,cnv_df.columns.isin(common_ids)]
     expr_df = expr_df.loc[:,expr_df.columns.isin(common_ids)]
+    snv_df = snv_df[snv_df.p_id.isin(common_ids)]
+    cnv_df = cnv_df.loc[:,cnv_df.columns.isin(common_ids)]
 
-    return expr_df, mut_df, cnv_df
+    return expr_df, snv_df, cnv_df
 
-#pog_tpm, pog_snv = find_smpls_w_expr_n_mut_n_cnv_data(pog_tpm, pog_snv)
-tcga_tpm, tcga_snv, cnv_df = find_smpls_w_expr_n_mut_n_cnv_data(tcga_tpm, tcga_snv)
-tcga_tpm_primary_only, tcga_snv, cnv_df = find_smpls_w_expr_n_mut_n_cnv_data(tcga_tpm_primary_only, tcga_snv)
-#hartwig_tpm, hartwig_snv = find_smpls_w_expr_n_mut_n_cnv_data(hartwig_tpm, hartwig_snv)
+pog_tpm, pog_snv, pog_cnv = find_smpls_w_expr_n_mut_n_cnv_data(pog_tpm, pog_snv, pog_cnv)
+tcga_tpm, tcga_snv, tcga_cnv = find_smpls_w_expr_n_mut_n_cnv_data(tcga_tpm, tcga_snv, tcga_cnv)
 
-# harmonize somatic mutation data for tcga and pog
-pog_snv = pog_snv[['p_id','gene_name','effect']]
-tcga_snv = tcga_snv[['p_id','gene_name','One_Consequence']]
-
-pog_snv = pog_snv.rename(columns={'effect':'consequence'})
-tcga_snv = tcga_snv.rename(columns={'One_Consequence':'consequence'})
+# select required columns from somatic mutation data for tcga and pog 
+pog_snv = pog_snv[['p_id','gene_name','consequence','base_change','amino_acid_change']]
+tcga_snv = tcga_snv[['p_id','gene_name','consequence','base_change','amino_acid_change']]
 
 #######################################################
 ######### Process Germline Mutation Datasets ##########
 #######################################################
 
 # extract required columns from germline mutation data
-tcga_germline = tcga_germline[['Sample','HUGO_Symbol','Consequence','Overall_Classification']]
-pog_germline = pog_germline[['pog_id','gene_name','function','reviewed_classification']]
+tcga_germline = tcga_germline[['Sample','HUGO_Symbol','Consequence','Overall_Classification','HGVSc','HGVSp']]
+pog_germline = pog_germline[['pog_id','gene_name','function','reviewed_classification','refseq_aa_change']]
 
-# harmonize column names in germline mutation data
-tcga_germline = tcga_germline.rename(columns={'Sample':'p_id','HUGO_Symbol':'gene_name','Consequence':'consequence','Overall_Classification':'classification'})
+# extract base change and aa change from refseq_aa_change column in pog data
+pog_germline['base_change'] = pog_germline.refseq_aa_change.str.split(':').str[3]
+pog_germline['amino_acid_change'] = pog_germline.refseq_aa_change.str.split(':').str[4]
+
+# harmonize ids between tcga somatic and germline mut data
+tcga_germline['p_id'] = tcga_germline.Sample.str.slice(0,15)
+tcga_germline = tcga_germline.drop(columns=['Sample'])
+
+# rename columns in germline mutation data
+tcga_germline = tcga_germline.rename(columns={'HUGO_Symbol':'gene_name','Consequence':'consequence','Overall_Classification':'classification'
+                                              , 'HGVSc':'base_change', 'HGVSp':'amino_acid_change'})
 pog_germline = pog_germline.rename(columns={'pog_id':'p_id','function':'consequence','reviewed_classification':'classification'})
+tcga_germline = tcga_germline.drop_duplicates()
+pog_germline = pog_germline.drop_duplicates()
 
-# filter germline data for pathogenic or likely pathogenic mutations and add to somatic mut data
+# extract pathogenic and likely pathogenic germline mutation data and add them to SNV data
+# (to make sure all samples with disruptive mutations are labeled as mutant irrespective of the source)
 def filter_and_add(germline_mut_df, somatic_mut_df):
     germline_mut_df = germline_mut_df[(germline_mut_df.classification == 'Pathogenic') | 
                                       (germline_mut_df.classification == 'Likely Pathogenic') | 
@@ -182,10 +176,14 @@ pog_all_mut = pog_all_mut.reset_index(drop=True)
 ######### Process Structural Variation Datasets ##########
 ##########################################################
 
-# filter SV data for needed columns and samples with SVs in the gene of interest
-tcga_sv_fltrd = tcga_sv[['Sample_Id','Site1_Hugo_Symbol','Site2_Hugo_Symbol','Site2_Effect_On_Frame','Event_Info']]
-tcga_sv_gene_of_interest = tcga_sv_fltrd[(tcga_sv_fltrd.Site1_Hugo_Symbol==gene_of_interest) | 
-                                         (tcga_sv_fltrd.Site2_Hugo_Symbol==gene_of_interest)]
+# extract required columns
+tcga_sv = tcga_sv[['Sample_Id','Site1_Hugo_Symbol','Site2_Hugo_Symbol']]
+pog_sv = pog_sv[['Patient Participant_project_identifier','Gene1_aliases','Gene2_aliases']]
+pog_sv = pog_sv.drop_duplicates()
+
+# harmonize the column names
+tcga_sv = tcga_sv.rename(columns={'Sample_Id':'p_id', 'Site1_Hugo_Symbol':'gene1', 'Site2_Hugo_Symbol':'gene2'})
+pog_sv = pog_sv.rename(columns={'Patient Participant_project_identifier':'p_id','Gene1_aliases':'gene1', 'Gene2_aliases':'gene2'})
 
 print('Data has been processed . . .')
 print('')
@@ -194,7 +192,15 @@ print('')
 ##### Write processed files into tmp directory #####
 ####################################################
 
-tcga_tpm.to_csv(snakemake.output.tcga_expr_prcssd, sep='\t', index=False)
+pog_tpm.to_csv(snakemake.output.pog_tpm_prcssd, sep='\t', index=False)
+pog_cnv.to_csv(snakemake.output.pog_cnv_prcssd, sep='\t', index=False)
+pog_all_mut.to_csv(snakemake.output.pog_mut_prcssd, sep='\t', index=False)
+pog_sv.to_csv(snakemake.output.pog_sv_prcssd, sep='\t', index=False)
+
+tcga_tpm.to_csv(snakemake.output.tcga_tpm_prcssd, sep='\t', index=False)
+tcga_cnv.to_csv(snakemake.output.tcga_cnv_prcssd, sep='\t', index=False)
+tcga_all_mut.to_csv(snakemake.output.tcga_mut_prcssd, sep='\t', index=False)
+tcga_sv.to_csv(snakemake.output.tcga_sv_prcssd, sep='\t', index=False)
 
 print('Processed files were written into tmp directory!')
 print('')
